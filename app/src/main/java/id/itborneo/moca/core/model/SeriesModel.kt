af63ae89c860c0f06011a19d1c3e05cf5591c545
Irpan <irpant1998@gmail.com>
2021-04-19 11:10:24 +0800

    19 April - finished for sub2

--- app/src/main/java/id/itborneo/moca/core/model/SeriesModel.kt
+++ app/src/main/java/id/itborneo/moca/core/model/SeriesModel.kt
@@ -6,30 +6,14 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class SeriesModel(
-    @SerializedName("backdrop_path") 
-    var backdropPath: String? = null,
-    @SerializedName("first_air_date")
-    var firstAirDate: String? = null,
-    @SerializedName("genre_ids")
-    var genreIds: List<Int>? = null,
-    @SerializedName("id")
+
+    @field:SerializedName("id")
     var id: Int? = null,
+
     @SerializedName("name")
-    var name: String?= null,
-    @SerializedName("origin_country")
-    var originCountry: List<String>?= null,
-    @SerializedName("original_language")
-    var originalLanguage: String?= null,
-    @SerializedName("original_name")
-    var originalName: String?= null,
-    @SerializedName("overview")
-    var overview: String?= null,
-    @SerializedName("popularity")
-    var popularity: Double?= null,
+    var name: String? = null,
+
     @SerializedName("poster_path")
-    var posterPath: String?= null,
-    @SerializedName("vote_average")
-    var voteAverage: Double?= null,
-    @SerializedName("vote_count")
-    var voteCount: Int?= null
+    var posterPath: String? = null
+
 ) : Parcelable
--- app/src/main/java/id/itborneo/moca/core/model/credits/CreditsModel.kt
+++ app/src/main/java/id/itborneo/moca/core/model/credits/CreditsModel.kt
@@ -5,8 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class CreditsModel(
     @SerializedName("cast")
     var cast: List<CastModel>?,
-    @SerializedName("crew")
-    var crew: List<CrewModel>?,
+
     @SerializedName("id")
     var id: Int?
 )
