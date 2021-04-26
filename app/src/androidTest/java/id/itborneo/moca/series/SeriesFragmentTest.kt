80373f9f9f315f3294de47039a537cb2111e27a9
Irpan <irpant1998@gmail.com>
2021-04-26 11:37:01 +0800

    update readme

--- app/src/androidTest/java/id/itborneo/moca/series/SeriesFragmentTest.kt
+++ app/src/androidTest/java/id/itborneo/moca/series/SeriesFragmentTest.kt
@@ -37,7 +37,7 @@ class SeriesFragmentTest {
         //click to series fragment
         Espresso.onView(ViewMatchers.withId(R.id.seriesFragment)).perform(ViewActions.click())
 
-        val position = 3
+        val position = 4
         val recyclerViewTest = Espresso.onView(ViewMatchers.withId(R.id.rv_series))
 
         //show recyclerview
